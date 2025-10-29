fbc8591796accef5d1992b8ac7a846d7dbaf8d4d
Conscience-Constellator <66545800+Conscience-Constellator@users.noreply.github.com>
2025-10-29 17:46:04 -0400

    Add files via upload

---
+++ CC.$.1$1.Bind_B/Point2$Bool_B.java
@@ -0,0 +1,30 @@
+package CC.$.Q$Q.Bind_B;
+
+//import CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Rap.Clas_Rap;
+//import static CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Stat_Notif.*;
+
+import CC.$.Q$Q.Point$Bool;
+import CC.$.Q2$1.Point2$Bool;
+
+public class Point2$Bool_B<A_Typ,B_Typ>
+	implements Point$Bool<A_Typ>
+{
+	/*
+	public static final Clas_Rap Class=Init_StRt(Point2_Bool_A.class,
+		Clas_Rap.class);
+	/*Dep ?done*/
+
+	public Point2$Bool<A_Typ,B_Typ> Rapd;
+		public B_Typ B;
+		@Override
+		public boolean Point$Bool(A_Typ In)
+		{return Rapd.Point2$Bool(In,B);}
+
+	public Point2$Bool_B(Point2$Bool<A_Typ,B_Typ> Rapd,B_Typ B)
+	{
+		this.Rapd=Rapd;
+		this.B=B;
+	}
+
+//	static{Init_Nd(Point2_Bool_A.class);}
+}
